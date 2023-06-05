f0f41d7c656f2f56c2ae1bf8c4d8870d73437dbb
Vishal Dombale <79363597+vishaldombale08@users.noreply.github.com>
2023-06-05 14:07:27 +0530

    Add files via upload

---
+++ Assignment 1 - Web Development Using Python.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1. Write a command to get the Python version you are using.
+import sys
+print(sys.version)
+
+
+# In[4]:
+
+
+#2. Write a python script to print Hello Python on the screen directly in the command line.
+print("Hello Python")
+
+
+# #3. Write a python script and store it in a file and execute the file from the command line.
+# #input
+# C:\Users\Vishal>python "E:\Ineuron\Web Development using Python\Python Codes\Web Development Using Python All Assignment\Assignment 1 - Web Development Using Python - Done.py"
+# 
+# #output
+# 
+# #will get output
+
+# In[5]:
+
+
+#4. Write a python script to print Hello Python on the screen using visual studio code.
+
+#same as followed above question
+
+print("Hello Python")
+
+
+# In[6]:
+
+
+#5. Write a python script to print Hello on the first line and Python on the second line
+
+print("Hello")
+print("Pyhton")
+
+
+# In[8]:
+
+
+#5. Write a python script to print Hello on the first line and Python on the second line
+
+print("Hello", "Python", sep="\n")
+
+
+# In[11]:
+
+
+#6. Write a python script to print “MySirG” on the screen.
+
+print("\"MySirG\"")
+
+
+# In[12]:
+
+
+#7. Write a python script to print “Teacher’s Day” on the screen
+
+print("\"Teacher's Day\"")
+
+
+# In[17]:
+
+
+#8. Write a python script to print the value of a variable. Variable contains your name as data.
+
+Name = "Vishal Dadaso Dombale"
+print(Name)
+
+
+# In[5]:
+
+
+#9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+
+a = "Vishal Dadaso Dombale"
+b = 23
+c = "B.E(Artificial Intelligence and Machine Learning)"
+d = 3
+print(a,b,c,d, sep=("\n"))
+
+
+# In[6]:
+
+
+#10. Write a python script to print values of three variables, each in a new line. Variable contains integer values
+
+a = 8
+b = 12
+c = 20
+print(a,b,c, sep=("\n"))
+
